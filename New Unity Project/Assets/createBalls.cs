0da998e027138f6ca6f9206370e1ed5283a03758
Insominis <erickaylin@gmail.com>
2016-10-23 19:00:44 -0400

    Bouncing Balls
    Generates bouncy balls in Unity

---
+++ New Unity Project/Assets/createBalls.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createBalls : MonoBehaviour {
+    public Rigidbody sphere;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown (0)) //when mouse is clicked
+        {
+            var mousePos = Input.mousePosition; //gets position of mouse
+            mousePos.z = 10; //10m away from the camera
+            var camPos = Camera.main.ScreenToWorldPoint(mousePos); //moves main camera to position of mouse
+            GameObject bouncyBall = (GameObject)Instantiate(sphere, camPos, Quaternion.identity); //Casts sphere to GameObject.
+            //Instantiates rigidbody object sphere at objectPos. Still receive cast error.
+        }
+    }
+}
